e7a425882c822686814c451308b756cbd5efd74e
Megan Brown <mbrown@rosovconsulting.com>
2025-06-05 14:45:51 -0700

    filter incompletes and select columns and cleaned file to .rds

--- jimena_parent_survey_cleaning.R
+++ jimena_parent_survey_cleaning.R
@@ -4,13 +4,21 @@ library(janitor)
 library(summarytools)
 library(labelled)
 
+
 view_summary_browser <- function(data, name = deparse(substitute(data))){
   summarytools::view(summarytools::dfSummary(data), method = "browser", report.title = name)
 }
 
 
 ## Read in .sav file downloaded from Qualtrics
-raw_survey_data <- read_sav("Data/277-04_Jimena_RAW_Parent_Survey_data_20250605.sav") %>% 
+raw_survey_data <- read_sav("Data/277-04_JIMENA_RAW_Parent_Survey_data_20250605.sav") %>% 
   clean_names() %>% 
   as_factor()
+
+survey_data_1 <- raw_survey_data %>% 
+  filter(progress > 70) %>% 
+  select(response_id, school, q1:q3_4)
+
+
+write_rds(survey_data_1, "Data/277-04_JIMENA_Cleaned_Parent_Survey_Data_20250605.rds")
 
